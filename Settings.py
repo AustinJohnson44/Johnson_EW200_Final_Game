9724581d14ea4f40b53905377893ef7116d42c28
AustinJohnson44 <m263126@usna.edu>
2023-10-26 09:44:01 -0400

    created background with plain and textured grass - semi works

---
+++ Settings.py
@@ -0,0 +1,9 @@
+# Dimensions
+TILE_SIZE = 16  # tiles are square so height==width
+SCREEN_WIDTH = 52*TILE_SIZE
+SCREEN_HEIGHT = 40*TILE_SIZE
+TITLE_SIZE = 128
+
+# Colors
+
+# GAME DYNAMICS
--- game.py
+++ game.py
@@ -1,3 +1,50 @@
 import pygame
+import sys
+import random
+from settings import *
 
-print("Hello World!")
+pygame.init()  # tells pygame to look/listen for inputs and events
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,
+                                  SCREEN_HEIGHT))  # collapsed variables inside parenthesis
+background = screen.copy()  # makes a second copy of the screen/canvas
+clock = pygame.time.Clock()
+
+plain_grass = pygame.image.load("assets/tiles/plain_grass.png").convert()
+plain_grass.set_colorkey((0, 0, 0))
+textured_grass = pygame.image.load("assets/tiles/textured_grass.png").convert()
+textured_grass.set_colorkey((0, 0, 0))
+
+
+def draw_background():
+    background.fill((0, 0, 0))
+    # make a grass bottom
+    for h in range(SCREEN_HEIGHT // TILE_SIZE):
+        for i in range(SCREEN_WIDTH // TILE_SIZE):  # // to give quotient and not a float
+            background.blit(plain_grass, (TILE_SIZE * i, SCREEN_HEIGHT - TILE_SIZE*(h + 1)))
+
+    for h in range(SCREEN_HEIGHT // TILE_SIZE):
+        for i in range(random.randint(0, SCREEN_WIDTH // TILE_SIZE)):
+            x = random.randint(0, 11) * TILE_SIZE
+            y = SCREEN_HEIGHT - TILE_SIZE*(h + 1)
+            background.blit(textured_grass, (x, y))
+
+
+draw_background()
+
+while True:
+
+    # listen for events
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            print("Thanks for playing!")
+            pygame.quit()  # stops process that pygame.init started
+            sys.exit()  # uber break - breaks out of everything
+
+    # draw game screen
+    screen.blit(background, (0, 0))
+
+    pygame.display.flip()
+    clock.tick(60)  # locks game to 60fps
+
