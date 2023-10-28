a5ffdfd0a941381a03114414d11303b86c68e4b2
AustinJohnson44 <m263126@usna.edu>
2023-10-27 21:44:19 -0400

    changed grid and added the robber to the game

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import sys
 import random
 import safe_house
 import bank
+import people
 from building import Building, city
 from settings import *
 
@@ -25,16 +26,16 @@ vertical_road = pygame.transform.scale(v_road,
                                        (v_road.get_width() * 2, v_road.get_height() * 2))
 vertical_road.set_colorkey((0, 0, 0))
 
-# create safe house in bottom left corner
-my_safe_house = safe_house.SafeHouse(CITY_LEFT, CITY_BOTTOM)
-# create a bank in the top right corner
-my_bank = bank.Bank(CITY_RIGHT, CITY_TOP)
 # create city buildings
-for a in range((CITY_TOP // TILE_SIZE) + 5, CITY_BOTTOM // TILE_SIZE, 7):
-    for b in range(0, (SCREEN_WIDTH // TILE_SIZE), 7):  # // to give quotient and not a float
+for a in range(0, SCREEN_HEIGHT // TILE_SIZE, 7):
+    for b in range(0, (SCREEN_WIDTH // TILE_SIZE), 7):
         city.add(Building(TILE_SIZE * b, TILE_SIZE * a))
-for c in range(0, (CITY_RIGHT // TILE_SIZE), 7):
-    city.add(Building(TILE_SIZE * c, 0))
+# create safe house in bottom left corner
+my_safe_house = safe_house.SafeHouse(0, SCREEN_HEIGHT - 4 * TILE_SIZE)
+# create a bank in the top right corner
+my_bank = bank.Bank(SCREEN_WIDTH - 3 * TILE_SIZE, 0)
+# create a robber on the screen
+my_robber = people.Robber(4 * TILE_SIZE, SCREEN_HEIGHT - 4 * TILE_SIZE)
 
 
 background = screen.copy()  # makes a second copy of the screen/canvas
@@ -54,17 +55,18 @@ def draw_background():
             background.blit(textured_grass, (x, y))
     # make road system
     # make horizontal roads
-    for h in range(3 * TILE_SIZE, CITY_BOTTOM, 7 * TILE_SIZE):
+    for h in range(4 * TILE_SIZE, SCREEN_HEIGHT, 7 * TILE_SIZE):
         for i in range(0, SCREEN_WIDTH, TILE_SIZE):
             x = i
             y = h
             background.blit(horizontal_road, (x, y))
     # make vertical roads
-    for i in range(3 * TILE_SIZE, SCREEN_WIDTH, 7 * TILE_SIZE):
-        for h in range(0, CITY_BOTTOM, TILE_SIZE):
+    for i in range(4 * TILE_SIZE, SCREEN_WIDTH, 7 * TILE_SIZE):
+        for h in range(0, SCREEN_HEIGHT, TILE_SIZE):
             x = i
             y = h
             background.blit(vertical_road, (x, y))
+
 
 draw_background()
 
@@ -78,13 +80,39 @@ while True:
             pygame.quit()  # stops process that pygame.init started
             sys.exit()  # uber break - breaks out of everything
 
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                my_robber.moving_left = True
+            if event.key == pygame.K_RIGHT:
+                my_robber.moving_right = True
+            if event.key == pygame.K_UP:
+                my_robber.moving_up = True
+            if event.key == pygame.K_DOWN:
+                my_robber.moving_down = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                my_robber.moving_left = False
+            if event.key == pygame.K_RIGHT:
+                my_robber.moving_right = False
+            if event.key == pygame.K_UP:
+                my_robber.moving_up = False
+            if event.key == pygame.K_DOWN:
+                my_robber.moving_down = False
+
+    # update game objects
+    my_robber.update()
+
+    # check for collisions
+    #pygame.sprite.spritecollide(my_safe_house, city, True)
+    #pygame.sprite.spritecollide(my_bank, city, True)
+
     # draw game screen
     screen.blit(background, (0, 0))
 
+    city.draw(screen)
     my_safe_house.draw(screen)
     my_bank.draw(screen)
-
-    city.draw(screen)
+    my_robber.draw(screen)
 
     pygame.display.flip()
     clock.tick(60)  # locks game to 60fps
